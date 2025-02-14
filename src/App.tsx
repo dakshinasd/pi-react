48ebf8a295139660c65ff30d4667b04e897f0cc2
Dakshina Wijesuriya <dakshinasd@gmail.com>
2025-02-14 16:33:35 +0530

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>This is the modification</h1>
+      <h1 className="text- xl font-black">This is the modification</h1>
     </>
   );
 }
